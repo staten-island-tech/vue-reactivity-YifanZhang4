34edaf049361bf0da5ad92542ec657b91784e95b
YifanZhang4 <bookworm.with.a.problem@gmail.com>
2023-03-17 13:15:36 -0400

    button created? does not work

---
+++ vue-react/src/components/ObtainedButton.vue
@@ -0,0 +1,17 @@
+<template>
+  <button v-if="got === true" @click="change()">Not Got</button>
+  <button v-if="got === false" @click="change()">Got</button>
+</template>
+
+<script>
+export default {
+  name: 'ObtainedButton',
+  props: {
+    got: Boolean
+  }
+}
+function change() {
+  got.value = !got.value
+  console.log('got changed')
+}
+</script>
