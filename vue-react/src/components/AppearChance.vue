46c8dc4337cea34849590731d65589ed9b7c83ce
YifanZhang4 <bookworm.with.a.problem@gmail.com>
2023-03-31 13:18:07 -0400

    ??? I DONT GET IT

--- vue-react/src/components/AppearChance.vue
+++ vue-react/src/components/AppearChance.vue
@@ -1,7 +1,6 @@
 <template>
   <div id="foundMon"></div>
-  <button @click="$emit(findRandom)" id="find">Explore Tall Grass</button>
-  <button @click="test" id="run">Run Away</button>
+  <EncounteredPokemon />
   <button @click="catches" id="catch">Catch</button>
   <button @click="next" id="next" type="button">Onto the Next!</button>
   <img src="/src/assets/pokebal.gif" alt="Shaking PokeBall gif" id="gif" />
@@ -9,9 +8,11 @@
 </template>
 
 <script>
+import EncounteredPokemon from './EncounteredPokemon.vue'
 import PokeList from './PokeList.js'
+import RunAway from './RunAway.vue'
 export default {
-  components: {},
+  components: { EncounteredPokemon, RunAway },
   name: 'AppearChance',
   props: {},
   data() {
