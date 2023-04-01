5abd5feac675370263a6c4c8a2a63dc00cb28fc5
YifanZhang4 <bookworm.with.a.problem@gmail.com>
2023-03-31 20:14:14 -0400

    Revert "Revert "??? I DONT GET IT""
    This reverts commit e741944626870451bdbf9f3ade79929aad07cc2f.

---
+++ vue-react/src/components/EncounteredPokemon.vue
@@ -0,0 +1,14 @@
+<template>
+  <h2 id="text">{{ found.name }} has been found!</h2>
+  <RunAway />
+</template>
+
+<script>
+import found from './GetPokemon.vue'
+export default {
+  name: 'EncounteredPokemon',
+  props: {
+    found: found
+  }
+}
+</script>
